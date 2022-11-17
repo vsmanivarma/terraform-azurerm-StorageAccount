0f0e2c6c2d1932825971c13d3960d4e6809e6506
vsmanivarma <94699976+vsmanivarma@users.noreply.github.com>
2022-11-18 00:08:07 +0530

    Update main.tf

--- storageaccount/main.tf
+++ storageaccount/main.tf
@@ -8,9 +8,5 @@ terraform {
 }
 
 provider "azurerm" {
-client_id = "bc6d2044-28c4-4fdd-baba-bef1ad0afb6d"
-client_secret = "cDo8Q~twBn4R0wJ7G9NFiCBIZ_kfymb2nn3JrcOc"
-tenant_id = "3c725486-5265-4ceb-8094-cbb484f6cdaf"
-subscription_id = "32e51d6b-8566-49f4-9ea0-a48d4d6125ce"
  features {}
 }
